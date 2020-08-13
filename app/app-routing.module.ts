2ff2b11f45ca602e2d9482abef300aa9245a6172
Pedro Fukuda <pedrohfukuda@gmail.com>
2020-08-13 01:35:00 -0400

    Add files via upload
    Login de contas finalizado

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Router, RouterModule, Routes } from '@angular/router';
 //Componentes necessarios
 import { ContaComponent } from './conta/conta.component';
 import { DetalhesPerfilComponent } from './detalhes-perfil/detalhes-perfil.component';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { ListaContasComponent } from './lista-contas/lista-contas.component';
 
 const routes: Routes = [
 	{ path: 'conta/:id', component: ContaComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'conta/perfil/:id', component: DetalhesPerfilComponent },
+	{ path: 'allcontas', component: ListaContasComponent },
 	{ path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
@@ -21,7 +23,10 @@ export class AppRoutingModule {
 	constructor(private router: Router){}
 
 	RedirectConta(id: number){
-		this.router.navigate(['conta/${{id}}']);
-		console.log(id);
+		if (id === 99)
+			this.router.navigate(['allcontas']);
+		else if (id !== -1){
+			this.router.navigate(['conta/{{id}}']);
+		}
 	}
  }
